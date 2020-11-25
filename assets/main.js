154c31adc58d78c2f7f6cd0efd8ac6b2e4d5cf62
neillrobson <neillrobson@users.noreply.github.com>
2020-11-25 15:14:58 +0000

    deploy: 35a9f022e94ed679c8787d2d427b8a01991737af

---
+++ assets/main.js
@@ -0,0 +1,32 @@
+var duration = 15 * 1000;
+var animationEnd = Date.now() + duration;
+var skew = 1;
+
+function randomInRange(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+(function frame() {
+    var timeLeft = animationEnd - Date.now();
+    var ticks = Math.max(200, 500 * (timeLeft / duration));
+    skew = Math.max(0.8, skew - 0.001);
+
+    confetti({
+        particleCount: 1,
+        startVelocity: 0,
+        ticks: ticks,
+        gravity: 0.5,
+        origin: {
+            x: Math.random(),
+            // since particles fall down, skew start toward the top
+            y: (Math.random() * skew) - 0.2
+        },
+        colors: ['#888888'],
+        shapes: ['circle'],
+        scalar: randomInRange(0.4, 1)
+    });
+
+    if (timeLeft > 0) {
+        requestAnimationFrame(frame);
+    }
+}());
